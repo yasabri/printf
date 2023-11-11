5cc66231933a5eda57a0b11052676e252a711585
hamzas7 <hamza.yasr7@gmail.com>
2023-11-11 06:56:53 -0800

    printf

--- main.h
+++ main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <unistd.h>
 #include <stdio.h>
 
+int _strlen(char *str);
+int _strlenc(char *str)
+int printf_string(va_list val);
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _putchar(char c);
 
 #endif
---
+++ print_str.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @str: string pointer
+ * Return: i
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0 ; s[i] != 0 ; i++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strlenc - returns the length of a string
+ * @str: char pointer
+ * Return: i
+ */
+
+int _strlenc(const char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != 0 ; i++)
+		i++;
+	return (i);
+}
