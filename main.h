44788018ea726cff80e839d4d4758f444017d78b
hamzas7 <hamza.yasr7@gmail.com>
2023-11-11 07:04:02 -0800

    printf

--- main.h
+++ main.h
@@ -3,8 +3,9 @@
 #include <unistd.h>
 #include <stdio.h>
 
+int print_37(void);
 int _strlen(char *str);
-int _strlenc(char *str)
+int _strlenc(char *str);
 int printf_string(va_list val);
 int _printf(const char *format, ...);
 int _putchar(char c);
