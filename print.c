e900ed04b2405b270a4498624c05804bbb9a7d6f
yasabri <yasabri04@gmail.com>
2023-11-13 14:35:22 -0800

    print

--- print.c
+++ print.c
@@ -2,14 +2,14 @@
 
 
 /**
- * print_char - Prints a char
- * @types: List a of arguments
+ * print_char - char prints
+ * @types: arguments list
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: Width
  * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * @size: specifier size
+ * Return: chars Number printed
  */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
